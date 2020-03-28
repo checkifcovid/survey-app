877a9e624d8949dad40ed082013dc61a152efa41
zzmarkzz321 <markrenzosantiago@gmail.com>
2020-03-28 02:12:14 -0700

    wip

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,3 @@
 export { default as Button } from './Button/button';
 export { default as InputField } from './InputField/input-field'
 export { default as PageWrapper } from './PageWrapper/page-wrapper';
-
