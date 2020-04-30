b4db0f179e6b60f2902076a8636183cabf7ed34f
iwoork <efren@iwoork.com>
2020-04-29 22:52:50 -0400

    Added slack feedback form

--- components/layouts/default.js
+++ components/layouts/default.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import SlackFeedback from 'react-slack-feedback'
+
 import Menu from './menu'
 import Footer from './footer'
+
+const sendToServer = (payload, success, error) => {
+  return fetch('/api/slack', {
+    method: 'POST',
+    body: JSON.stringify(payload),
+  })
+    .then(success)
+    .catch(error)
+}
 
 const Layout = ({ children }) => (
   <>
     <Menu />
     {children}
     <Footer />
+    <SlackFeedback
+      channel="#feedback"
+      user="Slack Feedback" // The logged in user (default = "Unknown User")
+      onSubmit={(payload, success, error) =>
+        sendToServer(payload)
+          .then(success)
+          .catch(error)
+      }
+    />
+    ,
   </>
 )
 export default Layout
